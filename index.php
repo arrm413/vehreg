6a1ed5bd9fa2159ff95e41701f3fcda00b0fe3f6
arrm413 <arrm413@gmail.com>
2018-08-30 14:05:48 -0500

    fourth

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US"> 
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<title> 
+		Vehicle Registration Form 
+	</title>
+
+
+	<style>
+		.error {color: #FF0000;}
+	</style>
+
+	<!-- Include CSS for different screen sizes -->
+	<link rel="stylesheet" type="text/css" href="defaultstyle.css">
+</head>
+
+<body>
+
+<?php
+	
+	require 'connectToDatabase.php';
+
+	// Connect to Azure SQL Database
+	$conn = ConnectToDabase();
+
+	// Get data for expense categories
+	$tsql="SELECT * FROM VehicleReg";
+	$VehicleReg= sqlsrv_query($conn, $tsql);
+
+	// Populate dropdown menu options 
+	/*
+	$options = '';
+	while($row = sqlsrv_fetch_array($VehicleReg)) {
+		$options .="<option>" . $row['CATEGORY'] . "</option>";
+	}
+*/
+
+	// Close SQL database connection
+	sqlsrv_close ($conn);
+
+?>
+
+<div class="intro">
+
+	<h2> Vehicle Registration Form </h2>
+
+	<!-- Display redundant error message on top of webpage if there is an error -->
+	<h3> <span class="error"> <?php echo $prevSelections['errorMessage'] ?> </span> </h3>
+
+</div>
+
+<!-- Define web form. 
+The array $_POST is populated after the HTTP POST method.
+The PHP script insertToDb.php will be executed after the user clicks "Submit"-->
+<div class="container">
+	<form action="insertToDb.php" method="post">
+
+		<label>Vehicle Make:</label>
+		<input type="text" name="vehicle_make" >
+
+		<label>Vehicle Model:</label>
+		<input type="text" name="vehicle_model" >
+
+		<label>Vehicle Year:</label>
+		<input type="text" name="vehicle_year" >
+		
+		<label>Start Date:</label>
+		<input type="date" id="start" name="startdate"
+               			value="2018-08-30"
+              			min="2018-08-30" max="2199-12-31" />
+
+		<label>End Date:</label>
+		<input type="date" id="start" name="enddate"
+               			value="2018-08-30"
+              			min="2018-08-30" max="2199-12-31" />
+		<br><br>
+
+		<label>Employee Name:</label>
+		<input type="text" name="employee_name" >
+
+		<button type="submit">Submit</button>
+	</form>
+</div>
+
+</body>
+</html>
---
+++ insertToDb.php
@@ -0,0 +1,128 @@
+<?php
+
+	// Define function to handle basic user input
+	function parse_input($data) 
+	{
+		$data = trim($data);
+		$data = stripslashes($data);
+		$data = htmlspecialchars($data);
+		return $data;
+	}
+
+	// Define function to check that inputted expense number has a maximum of 2 decimal places
+	function validateTwoDecimals($number)
+	{
+	   return (preg_match('/^[0-9]+(\.[0-9]{1,2})?$/', $number));
+	}
+ 
+	// PHP script used to connect to backend Azure SQL database
+	require 'ConnectToDatabase.php';
+
+	// Start session for this particular PHP script execution.
+	session_start();
+
+	// Define ariables and set to empty values
+	$v_make = $v_model = $v_year = $start_dt = $end_dt = $name = $errorMessage = NULL;
+
+	// Get input variables
+	$v_make= parse_input($_POST['v_make']);
+	$v_model= parse_input($_POST['v_model']);
+	$v_year= parse_input($_POST['v_year']);
+	$start_dt= (date) parse_input($_POST['start_dt']);
+	$end_dt= (date) parse_input($_POST['end_dt']);
+	$name= parse_input($_POST['name']);
+
+	// Get the authentication claims stored in the Token Store after user logins using Azure Active Directory
+	$claims= json_decode($_SERVER['MS_CLIENT_PRINCIPAL'])->claims;
+	foreach($claims as $claim)
+	{		
+		if ( $claim->typ == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress" )
+		{
+			$userEmail= $claim->val;
+			break;
+		}
+	}
+
+	///////////////////////////////////////////////////////
+	//////////////////// INPUT VALIDATION /////////////////
+	///////////////////////////////////////////////////////
+
+	//Initialize variable to keep track of any errors
+	$anyErrors= FALSE;
+
+	// Check category validity
+	if ($name == '-1') {$errorMessage= "Error: Invalid Category Selected"; $anyErrors= TRUE;}
+/*	
+	// Check date validity
+	$isValidDate= checkdate($v_model, $v_make, $v_year);
+	if (!$isValidDate) {$errorMessage= "Error: Invalid Date"; $anyErrors= TRUE;}
+
+	// Check that the expense amount input has maximum of 2 decimal places (check against string input, not the float parsed input)
+	$isValidstart_dt= validateTwoDecimals(parse_input($_POST['start_dt']));
+	if (!$isValidstart_dt) {$errorMessage= "Error: Invalid Expense Amount"; $anyErrors= TRUE;}
+*/
+
+	///////////////////////////////////////////////////////
+	////////// INPUT PARSING AND WRITE TO SQL DB //////////
+	///////////////////////////////////////////////////////
+
+	// Only input information into database if there are no errors
+	if ( !$anyErrors ) 
+	{
+		// Create a DateTime object based on inputted data
+		//$dateObj= DateTime::createFromFormat('Y-m-d', $v_year . "-" . $v_model . "-" . $v_make);
+
+		// Get the name of the month (e.g. January) of this expense
+		//$v_modelName= $dateObj->format('F');
+
+		// Get the day of the week (e.g. Tuesday) of this expense
+		//$v_makeOfWeekNum= $dateObj->format('w');
+		//$days = array('Sunday', 'Monday', 'Tuesday', 'Wednesday','Thursday','Friday', 'Saturday');
+		//$v_makeOfWeek = $days[$v_makeOfWeekNum];
+
+		// Connect to Azure SQL Database
+		$conn = ConnectToDabase();
+
+		// Build SQL query to insert new expense data into SQL database
+		$tsql=
+		"INSERT INTO Expenses (	
+				v_make,
+				v_model,
+				v_year,
+				start_dt,
+				end_dt,
+				name
+				)
+		VALUES ('
+				'" . $v_make . "', 
+				'" . $v_model . "', 
+				'" . $v_year . "', 
+				'" . $start_dt . "', 
+				'" . $end_dt . "',
+				'" . $name . "',)";
+
+		// Run query
+		$sqlQueryStatus= sqlsrv_query($conn, $tsql);
+
+		// Close SQL database connection
+		sqlsrv_close ($conn);
+	}
+/*
+	// Initialize an array of previously-posted info
+	$prevSelections = array();
+
+	// Populate array with key-value pairs
+	$prevSelections['errorMessage']= $errorMessage;
+	$prevSelections['prevv_make']= $v_make;
+	$prevSelections['prevv_model']= $v_model;
+	$prevSelections['prevv_year']= $v_year;
+	$prevSelections['prevname']= $name;
+	$prevSelections['prevstart_dt']= $start_dt;
+	$prevSelections['prevend_dt']= $end_dt;
+
+	// Store previously-selected data as part of info to carry over after URL redirection
+	$_SESSION['prevSelections'] = $prevSelections;
+*/
+	/* Redirect browser to home page */
+	header("Location: /"); 
+?>
